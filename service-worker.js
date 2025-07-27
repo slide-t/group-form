82aa29ad41a440fa1ff1258e041b8af4884ea1d9
slide-t <slideteckacademy@gmail.com>
2025-07-27 04:31:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'group-form-v2.5.4'; // update this version on changes
+const CACHE_NAME = 'group-form-v2.5.5'; // update this version on changes
 const urlsToCache = [
   './',
   './',
